80d81c555e3cb4b7053fe4d5d0413c5241d9b222
Jeff Ligon <jeff@r65labs.com>
2016-02-16 21:19:18 -0500

    Initial Commit

---
+++ ColorTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorTableView
+//
+//  Created by Jeff Ligon on 2/16/16.
+//  Copyright © 2016 Visceral Origami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ColorTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ColorTableView
+//
+//  Created by Jeff Ligon on 2/16/16.
+//  Copyright © 2016 Visceral Origami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
